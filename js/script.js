7d8004ec345e5f8e7e113e897f355315cbc0dfb6
cucuri <145088908+cucuri@users.noreply.github.com>
2023-10-04 17:36:43 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,3 +5,43 @@ $(".gnb_wrap li").mouseenter(function(){
 $("nav").mouseleave(function(){
     $(".submenu").stop().slideUp()
 })
+
+
+function openBoard(evt, boardName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("board");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" opacity", "");
+    }
+    document.getElementById(boardName).style.display = "block";
+    evt.currentTarget.className += " opacity";
+}
+
+$(function(){
+
+	
+	$(".right> li> .font0").click(function(){
+		$(this).next().slideToggle();
+		$( '.c p' ).toggleClass( 'ab' );
+	});
+	
+		$(".right> li> .font1").click(function(){
+		$(this).next().slideToggle();
+		$( '.d p' ).toggleClass( 'ab' );
+	});
+	
+
+
+});
+
+$(function(){
+	$(".right> li> ul").click(function(){
+		$(this).children().children("img").toggleClass("turn");
+	});
+});
+
+
